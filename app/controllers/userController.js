2bc85b47a55b9f2d0c44fe4e0b5d7b4d39886bfe
Joeri Smits <joeri.smits@planet.nl>
2014-11-07 03:32:25 +0100

    changed design login page

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -11,13 +11,21 @@ var mongoose = require('mongoose'),
 // ==============================================
 exports.checkLogin = function (req, res) {
     User.findOne({email: req.body.email}, function (err, user) {
+        // Check if the user exists in the database.
         if (user) {
-            console.log("User exist");
-            res.send({
-                message: "User exist"
-            })
+
+            // Check if the password is the same as the user sends
+            if (user.password === req.body.password) {
+                res.send({
+                    user: user
+                })
+            } else {
+                res.redirect("/home");
+                res.send({
+                    message: "Wrong password"
+                });
+            }
         } else {
-            console.log("User does not exist");
             res.send({
                 message: "User does not exist"
             })
